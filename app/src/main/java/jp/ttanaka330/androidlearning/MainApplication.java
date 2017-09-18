a1992caafd33e333a2898055c467a72818b6e6d8
ttanaka330 <tomyamkun330@gmail.com>
2017-09-18 10:57:42 +0900

    パッケージ変更

---
+++ app/src/main/java/jp/ttanaka330/androidlearning/MainApplication.java
@@ -0,0 +1,8 @@
+package jp.ttanaka330.androidlearning;
+
+/**
+ * Created by Tomohiro Tanaka on 2017/09/18.
+ */
+
+public class MainApplication {
+}
---
+++ app/src/main/java/jp/ttanaka330/androidlearning/di/AppComponent.java
@@ -0,0 +1,8 @@
+package jp.ttanaka330.androidlearning.di;
+
+/**
+ * Created by Tomohiro Tanaka on 2017/09/18.
+ */
+
+public class AppComponent {
+}
