18e8c48f00bd8782af3e8e9d0c6149534c0573ea
ttanaka330 <tomyamkun330@gmail.com>
2019-03-03 23:12:56 +0900

    Replace AndroidJUnit4
    * androidx.test.runner to androidx.test.ext.junit.runners

--- app/src/androidTest/java/jp/ttanaka330/androidlearning/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jp/ttanaka330/androidlearning/ExampleInstrumentedTest.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,10 +18,14 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
-        assertEquals("jp.tomyamkun330.androidlearning", appContext.getPackageName());
+        if (BuildConfig.DEBUG) {
+            assertEquals("jp.ttanaka330.androidlearning.debug", appContext.getPackageName());
+        } else {
+            assertEquals("jp.ttanaka330.androidlearning", appContext.getPackageName());
+        }
     }
 }
--- template/todo/src/androidTest/java/com/github/ttanaka330/learning/todo/ExampleInstrumentedTest.kt
+++ template/todo/src/androidTest/java/com/github/ttanaka330/learning/todo/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package com.github.ttanaka330.learning.todo
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,7 +17,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext: Context = ApplicationProvider.getApplicationContext()
         assertEquals("com.github.ttanaka330.learning.todo", appContext.packageName)
     }
 }
