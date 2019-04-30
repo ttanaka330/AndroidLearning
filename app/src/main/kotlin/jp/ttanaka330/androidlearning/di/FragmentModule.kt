a52676bf3183ffdcbc15f3d0c28f1cfd0b9f146f
ttanaka330 <tomyamkun330@gmail.com>
2019-04-30 21:48:37 +0900

    Remove old retrofit from app

--- app/src/main/kotlin/jp/ttanaka330/androidlearning/di/FragmentModule.kt
+++
@@ -1,6 +0,0 @@
-package jp.ttanaka330.androidlearning.di
-
-import dagger.Module
-
-@Module
-internal abstract class FragmentModule
--- app/src/main/kotlin/jp/ttanaka330/androidlearning/di/scope/FragmentScope.kt
+++
@@ -1,7 +0,0 @@
-package jp.ttanaka330.androidlearning.di.scope
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-annotation class FragmentScope
