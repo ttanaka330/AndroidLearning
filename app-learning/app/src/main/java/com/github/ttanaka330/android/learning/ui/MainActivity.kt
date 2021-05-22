a704a13ca3d3a694b356c3372486fcd12438d3ed
ttanaka330 <tomyamkun330@gmail.com>
2021-05-22 20:43:08 +0900

    Fix: Lint warning

--- app-learning/app/src/main/java/com/github/ttanaka330/android/learning/ui/MainActivity.kt
+++ app-learning/app/src/main/java/com/github/ttanaka330/android/learning/ui/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.github.ttanaka330.android.learning.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.ttanaka330.android.learning.R
+import androidx.appcompat.app.AppCompatActivity
 import com.github.ttanaka330.android.learning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
