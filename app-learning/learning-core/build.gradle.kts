fa7461f45dc0ca6859890303d6bbe382911373ed
ttanaka330 <tomyamkun330@gmail.com>
2025-06-25 00:12:04 +0900

    convert app-learning/build.gradle.kts

---
+++ app-learning/learning-core/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+apply(from = rootProject.file("gradle/android.gradle"))
+
+android {
+    namespace = "com.github.ttanaka330.android.learning"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            consumerProguardFiles("consumer-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.material)
+    testImplementation(libs.junit)
+}
